95ebafcbfa1da33caa44439f07452ac1e8e13599
feitian124 <feitian124@gmail.com>
2014-09-21 23:16:17 +0800

    a little refactor

--- express/index.js
+++ express/index.js
@@ -1,17 +1,14 @@
-// Setup basic express server
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
+var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io')(server),
+    sockets  = require('./sockets'),
+    port = process.env.PORT || 4201;
 
-var io = require('socket.io')(server);
-var port = process.env.PORT || 4201;
+app.use(express.static(__dirname + '/../dist'));
 
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
 });
 
-// Routing
-app.use(express.static(__dirname + '/../dist'));
-
-var sockets  = require('./sockets');
 sockets.on(io);
