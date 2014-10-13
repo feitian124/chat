7fb08ae5d3ebd43a33805a3bacfb97c193726725
feitian124 <feitian124@gmail.com>
2014-10-14 00:22:56 +0800

    sequelize+express example works

--- express/routes/index.js
+++ express/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var models  = require('../models');
 
-/* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  models.User.findAll({
+    include: [ models.Task ]
+  }).success(function(users) {
+    res.render('index', {
+      title: 'Express',
+      users: users
+    });
+  });
 });
 
 module.exports = router;
